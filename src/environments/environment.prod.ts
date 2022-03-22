796c8ef2dba487d14ce9b51232074da9bb208125
damiaofranca <francadamiao202@gmail.com>
2022-03-22 10:51:50 -0300

    adding variables in production enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCwnEYtdHGF9bFLMhOYTagIc5VnGANC53c",
+    authDomain: "air-condi.firebaseapp.com",
+    projectId: "air-condi",
+    storageBucket: "air-condi.appspot.com",
+    messagingSenderId: "300377946483",
+    appId: "1:300377946483:web:afd17855e2e5d39d4ded06",
+    measurementId: "G-75GQQ7FL5W"
+  },
+  api: 'https://api.rawg.io/api',
+  apiKey: '50fdeee156204b86b53c57a9b3d77125',
 };
