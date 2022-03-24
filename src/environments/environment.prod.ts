6d30dd0caa0bbec12c901e6e33e28aef32c0dcf8
Damião da Silva França <77007541+damiaofranca@users.noreply.github.com>
2022-03-24 11:21:46 -0300

    update enviroments keys in production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyCwnEYtdHGF9bFLMhOYTagIc5VnGANC53c",
-    authDomain: "air-condi.firebaseapp.com",
-    projectId: "air-condi",
-    storageBucket: "air-condi.appspot.com",
-    messagingSenderId: "300377946483",
-    appId: "1:300377946483:web:afd17855e2e5d39d4ded06",
-    measurementId: "G-75GQQ7FL5W"
+    apiKey: "AIzaSyDTrFOrph44NPtS8YnsPLJtc-X2DxIxUVc",
+    authDomain: "datawikigamer.firebaseapp.com",
+    databaseURL: "https://datawikigamer-default-rtdb.firebaseio.com",
+    projectId: "datawikigamer",
+    storageBucket: "datawikigamer.appspot.com",
+    messagingSenderId: "84650665753",
+    appId: "1:84650665753:web:9aef12e9be778b53984788"
   },
   api: 'https://api.rawg.io/api',
   apiKey: '50fdeee156204b86b53c57a9b3d77125',
